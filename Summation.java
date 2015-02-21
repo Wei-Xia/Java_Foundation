416f7bf83164301d26744bcba8a3b6074aec2d19
James Hsia <xw900812@gmail.com>
2015-02-21 12:24:50 -0500

    Add two numbers
    // program adds two given numbers together

---
+++ Summation.java
@@ -0,0 +1,16 @@
+// program adds two given numbers together 
+
+public class Summation{
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		int sum;
+		
+		num1 =30;
+		num2 =44;
+		
+		sum = num1 + num2;
+		
+		System.out.println( "The sum is " + sum);
+	}
+}
