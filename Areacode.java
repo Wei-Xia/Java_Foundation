c266457718db0695557693412814c5c1c85cddaf
James Hsia <xw900812@gmail.com>
2015-02-21 21:41:05 -0500

    Output the area code
    //Basic Java
    //Output the area code

---
+++ Areacode.java
@@ -0,0 +1,11 @@
+//Basic Java
+//Output the area code
+//"System.out.print" --continue
+//"System.out.println" -- next line
+
+public class Areacode {
+	public static void main(String[] args) {
+		System.out.print("the area code for");
+		System.out.println(" NY is " + 212 );
+	}
+}
