4586f51b08c37e37768fa584dfa6e1c6d611b8bf
James Hsia <xw900812@gmail.com>
2015-03-09 22:10:16 -0400

    Do Loop's Math question
    Find the result and check out at the end

---
+++ DoMathQuestion.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+
+public class DoMathQuestion {
+	public static void main(String[] args) {
+	
+	int x=5;
+	int y=5;
+	do{
+	if(x>2)
+	System.out.println("Lemon");
+	System.out.println("Apple");
+	x--;
+	y=y-2;
+	}while (x>0&&y>0);
+
+	}
+	}
+	
+	
+/* Answer:
+Lemon
+Apple
+Lemon
+Apple
+Lemon
+Apple
+*/
