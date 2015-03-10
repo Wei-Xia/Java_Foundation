3a48d20b2b2c00d1286c82829d776abb2641c3f7
James Hsia <xw900812@gmail.com>
2015-03-09 22:14:28 -0400

    For Loop Math Question
    Find out the answer at the end

---
+++ ForMathQuestion.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+
+public class ForMathQuestion {
+	public static void main(String[] args) {
+
+	
+	int sum=0;
+	for(int i=1; i <=3; i++)
+	for(int x=1; x <=2; x++)
+	sum= x+i;
+	System.out.println(sum);
+
+	}
+	}
+	
+	//answer: 5
