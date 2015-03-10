562a0c35ba6f2767aae570ea2b6cc3d3a46d720f
James Hsia <xw900812@gmail.com>
2015-03-09 23:17:07 -0400

    WhileIfMath
    check the answer at the bottom

---
+++ WhileIfMath.java
@@ -0,0 +1,20 @@
+public class WhileIfMath {
+	
+	public static void main(String[] args) {
+	
+	int i=10;
+	while (i>0){
+		if (i%2==0)
+	System.out.println(i);
+	i=i-2;}
+	
+	}
+	}
+	
+/* answer:
+10
+8
+6
+4
+2
+*/
