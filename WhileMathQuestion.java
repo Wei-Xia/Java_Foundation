d0020ef2f9d90d9fa2e54fddca07dcd41e59ba71
James Hsia <xw900812@gmail.com>
2015-03-09 22:15:06 -0400

    While Loop Math Question
    Find out the answer at the end

---
+++ WhileMathQuestion.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+
+public class WhileMathQuestion {
+	public static void main(String[] args) {
+
+	int i=0;
+	int sum=0;
+	while(i<=5){
+	sum=sum+i;
+	i++;}
+	System.out.println(sum);
+	}
+	}
+	
+	//answer: 15
