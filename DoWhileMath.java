cc74d0b265c2def7c21285016c5e8dc50e371612
James Hsia <xw900812@gmail.com>
2015-03-09 23:17:30 -0400

    Do While Math
    check the answer at the bottom

---
+++ DoWhileMath.java
@@ -0,0 +1,25 @@
+public class DoWhileMath {
+	
+	public static void main(String[] args) {
+	
+		int i = 1;
+     do {
+       System.out.println(i);
+       i++;
+     } while (i < 10);
+
+	}
+	}
+	
+/*
+answer:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
