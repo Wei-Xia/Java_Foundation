653e5fbc1fc184813f2e1f000ff203f30761cd60
Wei-Xia <xw900812@gmail.com>
2015-04-27 10:46:47 -0400

    Array to sum
    Basic array, sum a group of numbers

---
+++ Sum_Array.java
@@ -0,0 +1,13 @@
+public class Array {
+	public static void main(String[] args) {
+		int [] val = {0,1,2,4,16};
+		int sum=0;
+		for (int i=0; i<val.length; i++)
+		sum = sum + val [i];
+	System.out.println ("Sum = " + sum);
+		
+	}
+}
+
+
+// answer is 23
